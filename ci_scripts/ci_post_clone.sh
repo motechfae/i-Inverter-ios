2fdebc082ae81fff951f6cdb9feaf72854e69f9f
motechfae <motechfae@gmail.com>
2024-01-09 17:50:24 +0800

    modify ci_post_clone

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,8 +6,12 @@
 #  Created by motech fae on 2024/1/9.
 #
 
-# Install CocoaPods using Homebrew.
- brew install cocoapods
- 
-# Install dependencies you manage with CocoaPods.
- pod install
+echo "开始安装cocoapods"
+brew install cocoapods
+echo "cocoapods安装完毕"
+echo "开始设置cocoapods"
+pod setup
+echo "cocoapods设置完毕"
+echo "开始安装pods依赖库"
+pod install
+echo "pods依赖库安装完毕"
