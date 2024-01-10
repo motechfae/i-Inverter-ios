715dc4fbf98685c972178009831dd7b31c97a605
motechfae <motechfae@gmail.com>
2024-01-10 11:35:06 +0800

    upload ci_post_clone

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 
-#  ci_post_clone.sh
-#  i-Inverter
-#
-#  Created by motech fae on 2024/1/9.
-#
+# Install CocoaPods using Homebrew.
+ brew install cocoapods
 
-echo "开始安装cocoapods"
-brew install cocoapods
-echo "cocoapods安装完毕"
-echo "开始设置cocoapods"
-pod setup
-echo "cocoapods设置完毕"
-echo "开始安装pods依赖库"
-pod install
-echo "pods依赖库安装完毕"
+# Install dependencies you manage with CocoaPods.
+ pod install
